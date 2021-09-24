d241f54d2b8f79fa3ee1a55b46310656786be9e7
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-09-24 10:20:23 -0400

    3rd commit - adding: cleaning a little bit index.js

--- eCommerce/index.js
+++ eCommerce/index.js
@@ -4,8 +4,6 @@ const productsRouter = require('./api/resources/products/products.routes')
 
 const app = express()
 app.use(bodyParser.json())
-//const productsRouter = require('./products.routes')
-// Provisional DataBase...
 
 app.use('/api/products', productsRouter)
 
@@ -13,12 +11,9 @@ app.get('/', (req,res)=> {
     res.send('Welcome to my E-Commerce API BackEnd...')
 })
 
-// app.use('/api/products', productsRouter)
-
 app.listen(3000, ()=> {
     console.log('Server running at port 3000...')
 })
 
-// module.exports = app
 
 
