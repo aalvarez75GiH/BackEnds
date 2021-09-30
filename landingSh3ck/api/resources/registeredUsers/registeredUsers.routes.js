31c8a37669e72196450103d6cac96e6c2c41a936
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-09-30 00:37:18 -0400

    4th commit - adding: adding bcrypt and encrypting password for registered users

--- landingSh3ck/api/resources/registeredUsers/registeredUsers.routes.js
+++ landingSh3ck/api/resources/registeredUsers/registeredUsers.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const _ = require('underscore')
 const { v4: uuidv4 } = require("uuid")
+const bcrypt = require('bcrypt')
 
 const logger = require('../../../utils/logger')
 const registeredUsers = require('../../../database').registeredUsers
@@ -24,10 +25,22 @@ registeredUsersRouter.post('/', validateUsers, (req, res)=>{
         res.status(409).send(`User with email address: [${newRegisteredUser.email}] already exists`)
         return
     }
-    newRegisteredUser.id = uuidv4()    
-    registeredUsers.push(newRegisteredUser)   
-    logger.info(`User [${newRegisteredUser.email}] has been created...`)
-    res.status(201).send(`User [${newRegisteredUser.email}] has been created...`)
+    bcrypt.hash(newRegisteredUser.password, 10, ( err, hashedPassword )=> {
+        if (err){
+            logger.error('An error Ocurred when we try to get hash of user`s password', err)
+            res.status(500).send('An error ocurred processing user creation process')
+            return
+        }    
+        newRegisteredUser.id = uuidv4()
+        registeredUsers.push({
+            fullName:newRegisteredUser.fullName,
+            email:newRegisteredUser.email,
+            phoneNumber: newRegisteredUser.phoneNumber,
+            password: hashedPassword
+        })
+        logger.info(`User [${newRegisteredUser.email}] has been created...`)
+        res.status(201).send(`User [${newRegisteredUser.email}] has been created...`)
+    })
 })
 
 module.exports = registeredUsersRouter
--- landingSh3ck/index.js
+++ landingSh3ck/index.js
@@ -2,13 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const logger = require('./utils/logger')
 const morgan = require('morgan')
+const cors = require('cors')
+// const path = require('path')
 
 const countersRouter = require('./api/resources/counters/counters.routes')
 const usersRouter = require('./api/resources/users/users.routes')
 const registeredUsersRouter = require('./api/resources/registeredUsers/registeredUsers.routes')
 
 const app = express()
+// app.use(express.static(path.join('../../FrontEnds/landingsh3ck/','build')))
 app.use(bodyParser.json())
+app.use(cors())
 app.use(morgan('short', {
     stream: {
         write: message => logger.info(message.trim())
@@ -23,8 +27,11 @@ app.get('/', (req,res)=> {
     res.send('sh3ck has born today...')
 })
 
-app.listen(3000, () => {
-    logger.info('ch3ck server running at post 3000...')
-    // console.log('ch3ck server running at post 3000...')
+// app.get('*', (req,res)=>{
+//     res.sendFile(path.join('../../FrontEnds/landingsh3ck'+'/build/index.html'))
+// })
+
+app.listen(5000, () => {
+    logger.info('ch3ck server running at post 5000...')
 })
 
