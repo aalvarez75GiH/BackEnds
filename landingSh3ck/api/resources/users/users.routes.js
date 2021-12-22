ca7f1d77d0663dc3151aac6dcdd04e5e00478c6b
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-12-21 20:57:17 -0500

    52th commit - Changes: cleaning users.route.js

--- landingSh3ck/api/resources/users/users.routes.js
+++ landingSh3ck/api/resources/users/users.routes.js
@@ -42,17 +42,6 @@ usersRouter.post('/', [validateUsers, transformBodyToLowerCase], processingError
     const randomPIN = Math.floor(1000 + Math.random() * 9000)
     const PIN = randomPIN.toString()
     logger.info(PIN)
-    // bcrypt.hash(newUser.password, 10, async(error, hashedPassword) => {
-    //     if (error){
-    //         logger.info(`Error trying hashing password...`)
-    //         throw new ErrorHashingData()
-    //     }
-    //     await userController.createUser(newUser, hashedPassword, randomPIN)
-    //     logger.info(`User with email [${newUser.email}] has been created...`)
-    //     emailSender('users', newUser.email, randomPIN)
-    //     res.status(201).send(newUser.fullName)
-
-    // })
     bcrypt.hash(PIN, 10, async(error, hashedPIN) => {
         if (error){
             logger.info(`Error trying hashing PIN...`)
@@ -68,41 +57,6 @@ usersRouter.post('/', [validateUsers, transformBodyToLowerCase], processingError
 }))
 
 
-// ****************************** with async/await
-// usersRouter.post('/login', [validateLoginRequest, transformBodyToLowerCase], processingErrors(async(req,res) => {
-//     const notAuthUser = req.body
-//     let foundUser
-
-//     foundUser = await userController.findUserForLogin({ email: notAuthUser.email })    
-    
-//     if (!foundUser){
-//         logger.info(`User with email ${notAuthUser.email} was not found at DB`)
-//         res.status(400).send(`${notAuthUser.email}`)
-//         return
-//     }
-//     console.log('lo consiguió...')
-//     const hashedPassword = foundUser.password
-//     let correctPassword
-
-//     correctPassword = await bcrypt.compare(notAuthUser.password, hashedPassword)
-    
-//     if(correctPassword){
-//         console.log('password correcto...')
-//         const token = jwt.sign({id: foundUser.id},
-//         config.jwt.secret, {
-//             expiresIn: 60 * 60 * 24 * 365
-//         })
-//         logger.info(`User [${notAuthUser.email}] has been authenticated succesfully...`)
-//         res.status(200).send({token})
-//         return        
-//     }else{
-//         logger.info(`User with email ${notAuthUser.email} didn't complete authentication process`)
-//         res.status(400).send(`${foundUser.fullName}`)     
-//     }
-// }))
-
-
-// using PIN
 usersRouter.post('/login', [validateLoginRequest, transformBodyToLowerCase], processingErrors(async(req,res) => {
     const notAuthUser = req.body
     let foundUser
